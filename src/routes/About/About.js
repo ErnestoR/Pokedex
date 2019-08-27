291b42fad8b547005eb2bbf691bef72b2e6a1f4d
Ernesto Ramirez <eramirez@sciencelogic.com>
2019-08-27 15:27:27 -0500

    minor changes to about

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -3,12 +3,20 @@ import { Paper, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 
 const builtWith = [
-  {
-    name: "React"
-  },
-  {
-    name: "Material-UI"
-  }
+  { name: "@material-ui/core", varsion: "^4.3.3" },
+  { name: "@material-ui/icons", varsion: "^4.2.1" },
+  { name: "@material-ui/styles", varsion: "^4.3.3" },
+  { name: "clsx", varsion: "^1.0.4" },
+  { name: "react", varsion: "^16.9.0" },
+  { name: "react-dom", varsion: "^16.9.0" },
+  { name: "react-redux", varsion: "^7.1.1" },
+  { name: "react-router-dom", varsion: "^5.0.1" },
+  { name: "react-router-transition", varsion: "^1.3.0" },
+  { name: "react-scripts", varsion: "3.1.1" },
+  { name: "react-window", varsion: "^1.8.5" },
+  { name: "redux", varsion: "^4.0.4" },
+  { name: "redux-observable", varsion: "^1.1.0" },
+  { name: "rxjs", varsion: "^6.5.2" }
 ];
 
 const About = props => {
