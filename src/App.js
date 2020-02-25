d4a48acd1a2d239ea9517d86d21fe888da27ff35
Ernesto Ramirez <eramirez@sciencelogic.com>
2020-02-25 13:12:41 -0600

    switched to HashRouter and added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { CssBaseline } from "@material-ui/core";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 
 import theme from "./theme";
